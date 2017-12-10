c275b9eee62f6ba756c41bcdfc56e472df945389
moontoon <moon.shresth@gmail.com>
2017-12-10 20:05:55 +0545

    to calculate the value of acceleration

---
+++ acceleration.py
@@ -0,0 +1,11 @@
+# to find the value of acceleration from the given equation v = u + at
+
+v = 25
+u = 0
+t = 10
+
+print("The given values of final velocity, initial velocity and time are {0}, {1}, {2}". format(v, u, t))
+
+a = (v - u)/t
+
+print("The calculated acceleration is {}" . format(a))
